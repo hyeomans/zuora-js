3abd683747ff7e0f62052477d4bf4a051dd45a57
Hector Yeomans <yeomans.leonel@gmail.com>
2020-01-14 06:45:24 -0700

    Adding Basic Auth provider

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
 const axios = require('axios')
+const { bearerTokenProvider, basicAuthProvider } = require('./authHeaderProvider')
 
-const bearerTokenProvider = (oauth) => {
-  return (trackId) => {
-    return oauth.token(trackId).then(r => {
-      const { accessToken } = r
-      return `Bearer ${accessToken}`
-    })
+/**
+ * Creates an instance of a Zuora API
+ * @param {string} baseURL Zuora API URL
+ * @param {string} clientId Client ID or Email if using Basic Auth
+ * @param {string} clientSecret Client Secret or Email Password if using Basic Auth
+ * @param {boolean} useBasicAuth Use email/password instead of API credentials
+ */
+function zuora (baseURL, clientId, clientSecret, useBasicAuth = false) {
+  const axiosInstance = useBasicAuth ? axios.create({ baseURL: `${baseURL}:19016` }) : axios.create({ baseURL })
+
+  let authHeaderProvider
+  if (useBasicAuth) {
+    authHeaderProvider = basicAuthProvider(clientId, clientSecret)
+  } else {
+    const oauth = require('./oauth')(axiosInstance, clientId, clientSecret)
+    authHeaderProvider = bearerTokenProvider(oauth)
   }
-}
 
-function zuora (baseURL, clientId, clientSecret) {
-  const axiosInstance = axios.create({ baseURL })
-  const oauth = require('./oauth')(axiosInstance, clientId, clientSecret)
-  const bearerProvider = bearerTokenProvider(oauth)
   return {
-    oauth,
-    actions: require('./actions')(axiosInstance, bearerProvider),
-    describe: require('./describe')(axiosInstance, bearerProvider)
+    actions: require('./actions')(axiosInstance, authHeaderProvider),
+    describe: require('./describe')(axiosInstance, authHeaderProvider)
   }
 }
 
